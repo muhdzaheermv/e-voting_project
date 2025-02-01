a39fd5d8cc410c4fbb2e49398a6045c8c95c5538
muhammed zaheer <muhdzaheermv@gmail.com>
2025-02-02 02:41:22 +0530

    check repo

--- vote/urls.py
+++ vote/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),,
    path('',views.index,name='index'),
    path('about/',views.about,name='about'),
    path('guide/',views.guide,name='guide'),
